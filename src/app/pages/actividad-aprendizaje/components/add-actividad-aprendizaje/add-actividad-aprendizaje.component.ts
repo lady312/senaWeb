806eeb0c9c0f1cdbe79beabb03b288df1b4be91e
unknown <vanesagalindez318@gmail.com>
2023-06-08 11:39:41 -0500

    merg  idRap front 2

--- src/app/pages/actividad-aprendizaje/components/add-actividad-aprendizaje/add-actividad-aprendizaje.component.ts
+++ src/app/pages/actividad-aprendizaje/components/add-actividad-aprendizaje/add-actividad-aprendizaje.component.ts
@@ -19,7 +19,7 @@ export class AddActividadAprendizajeComponent implements OnInit {
   @Output() cancel: EventEmitter<void> = new EventEmitter();
 
   formAA: UntypedFormGroup;
-  resultadoAprendizajes: ResultadoAprendizajeModel[] = [];
+  resultadoAprendizaje: ResultadoAprendizajeModel[] = [];
 
   constructor(
     private formBuilder: UntypedFormBuilder,
@@ -46,7 +46,7 @@ export class AddActividadAprendizajeComponent implements OnInit {
   traerResultadoAprendizaje() {
     this.resultadoAprendizajeService.traerRap()
       .subscribe((proceso: ResultadoAprendizajeModel[]) => {
-        this.resultadoAprendizajes = proceso;
+        this.resultadoAprendizaje = proceso;
       }, error => {
         this._uiNotificationService.error('Error de conexión');
       });
