6f4af84df03c1b46ccc46425d8c6ee46c74711ac
unknown <vanesagalindez318@gmail.com>
2023-06-16 17:39:00 -0500

    ya funcionan los buscadores pr grup y aprend

--- src/app/pages/gestion-fichas/page/gestion-fichas/gestion-fichas.component.ts
+++ src/app/pages/gestion-fichas/page/gestion-fichas/gestion-fichas.component.ts
@@ -1,4 +1,3 @@
-// gestion-fichas.component.ts
 import { Component, OnInit } from '@angular/core';
 import { AsignacionParticipantesService } from '@services/asignacion-participantes.service';
 
@@ -20,6 +19,9 @@ export class GestionFichasComponent implements OnInit {
   gruposFiltrados: any[];
   aprendicesFiltrados: any[];
 
+  mostrarGrupos: boolean = true;
+  mostrarAprendices: boolean = false; // Variable para controlar la visibilidad del buscador de aprendices
+
   constructor(private asignacionParticipanteService: AsignacionParticipantesService) {}
 
   ngOnInit() {
@@ -28,47 +30,48 @@ export class GestionFichasComponent implements OnInit {
   }
 
   obtenerProgramas() {
-    this.asignacionParticipanteService.obtenerProgramas()
-      .subscribe(
-        programas => {
-          this.programas = programas;
-          this.programasFiltrados = programas;
-        },
-        error => console.log(error)
-      );
+    this.asignacionParticipanteService.obtenerProgramas().subscribe(
+      programas => {
+        this.programas = programas;
+        this.programasFiltrados = programas;
+      },
+      error => console.log(error)
+    );
   }
 
   obtenerGruposPorPrograma() {
     const programaSeleccionado = this.programas.find(programa => programa.nombrePrograma === this.busquedaPrograma);
 
     if (programaSeleccionado) {
-      this.asignacionParticipanteService.obtenerGruposPorPrograma(programaSeleccionado.id)
-        .subscribe(
-          grupos => {
-            this.grupos = grupos;
-            this.gruposFiltrados = grupos;
-            this.busquedaGrupo = ''; // Limpiar la búsqueda de grupos cuando se selecciona un programa
-            this.filtrarAprendicesPorGrupo(); // Filtrar los aprendices automáticamente
-          },
-          error => console.log(error)
-        );
+      this.asignacionParticipanteService.obtenerGruposPorPrograma(programaSeleccionado.id).subscribe(
+        grupos => {
+          this.grupos = grupos;
+          this.gruposFiltrados = grupos;
+          this.busquedaGrupo = ''; // Limpiar la búsqueda de grupos cuando se selecciona un programa
+          this.filtrarAprendicesPorGrupo(); // Filtrar los aprendices automáticamente
+          this.mostrarGrupos = this.gruposFiltrados && this.gruposFiltrados.length > 0;
+          this.mostrarAprendices = false; // Ocultar el buscador de aprendices al seleccionar un programa
+        },
+        error => console.log(error)
+      );
     } else {
       this.grupos = [];
       this.gruposFiltrados = [];
       this.busquedaGrupo = '';
       this.aprendicesFiltrados = [];
+      this.mostrarGrupos = false;
+      this.mostrarAprendices = false;
     }
   }
 
   obtenerAprendicesActivos() {
-    this.asignacionParticipanteService.obtenerAprendicesActivos()
-      .subscribe(
-        aprendices => {
-          this.aprendices = aprendices;
-          this.aprendicesFiltrados = aprendices;
-        },
-        error => console.log(error)
-      );
+    this.asignacionParticipanteService.obtenerAprendicesActivos().subscribe(
+      aprendices => {
+        this.aprendices = aprendices;
+        this.aprendicesFiltrados = aprendices;
+      },
+      error => console.log(error)
+    );
   }
 
   filtrarProgramas() {
@@ -90,26 +93,39 @@ export class GestionFichasComponent implements OnInit {
       this.gruposFiltrados = this.grupos;
     }
     this.filtrarAprendicesPorGrupo(); // Filtrar los aprendices automáticamente al cambiar la búsqueda de grupos
+
+    // Verificar si se encontraron grupos filtrados
+    this.mostrarGrupos = this.gruposFiltrados.length > 0;
+    this.mostrarAprendices = false; // Ocultar el buscador de aprendices al filtrar los grupos
   }
 
   filtrarAprendicesPorGrupo() {
     if (this.busquedaGrupo) {
       const grupoSeleccionado = this.grupos.find(grupo => grupo.nombre === this.busquedaGrupo);
       if (grupoSeleccionado) {
-        this.asignacionParticipanteService.obtenerAprendicesPorGrupo(grupoSeleccionado.id)
-          .subscribe(
-            aprendices => {
-              this.aprendicesFiltrados = aprendices;
-              console.log(this.aprendicesFiltrados)
-            },
-            error => console.log(error)
-          );
+        this.asignacionParticipanteService.obtenerAprendicesPorGrupo(grupoSeleccionado.id).subscribe(
+          aprendices => {
+            this.aprendicesFiltrados = aprendices;
+            this.filtrarAprendizPorIdentificacion(); // Filtro para identificación
+            this.mostrarAprendices = true; // Mostrar el buscador de aprendices cuando se encuentra un grupo
+          },
+          error => console.log(error)
+        );
       } else {
         this.aprendicesFiltrados = [];
-    
+        this.mostrarAprendices = false; // Ocultar el buscador de aprendices cuando no se encuentra un grupo
       }
     } else {
       this.aprendicesFiltrados = [];
+      this.mostrarAprendices = false; // Ocultar el buscador de aprendices cuando no se ha seleccionado un grupo
+    }
+  }
+
+  filtrarAprendizPorIdentificacion() {
+    if (this.busquedaAprendiz) {
+      this.aprendicesFiltrados = this.aprendicesFiltrados.filter(aprendiz =>
+        aprendiz.usuario.persona.identificacion.toLowerCase().includes(this.busquedaAprendiz.toLowerCase())
+      );
     }
   }
 }
