63bf700afd42254a5be40746722775bcd1dbd918
Jhon Wilmer Azcárate Rivera <unavidahonorable@hotmail.com>
2023-06-29 15:35:12 -0500

    Estabilizacion para produccion

--- src/app/pages/gestion-fichas/page/gestion-fichas.module.ts
+++ src/app/pages/gestion-fichas/page/gestion-fichas.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { GestionFichasRoutingModule } from './gestion-fichas-routing.module';
 import { GestionFichasComponent } from './gestion-fichas/gestion-fichas.component';
-import { GruposComponent } from '../../grupo/components/grupos/grupos.component';
 import { GruposListComponent } from '../../grupo/components/grupos-list/gruposList.component';
 import { GrupoComponent } from '../../grupo/pages/grupo/grupo.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -32,7 +31,6 @@ import { HttpClientModule } from '@angular/common/http';
 
   ],
   entryComponents:[
-    GruposComponent,
     GruposListComponent,
     GrupoComponent,
   ],
